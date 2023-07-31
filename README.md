56e7fbdede4b9da8f6d807e4c68f396bc9cb4dee
ryan he <ryanh7@163.com>
2023-07-31 21:06:10 +0800

    初始代码

---
+++ README.md
@@ -0,0 +1,4 @@
+# FFmpeg Camera
+[![hacs_badge](https://img.shields.io/badge/HACS-Custom-41BDF5.svg)](https://github.com/hacs/integration)
+
+适用于home assistant的天气插件，可通过hacs添加。
---
+++ custom_components/ha_ffmpeg_camera/const.py
@@ -0,0 +1,13 @@
+"""Constants for the generic (IP Camera) integration."""
+
+DOMAIN = "ha_ffmpeg_camera"
+DEFAULT_NAME = "FFmpeg Camera"
+CONF_CONFIRMED_OK = "confirmed_ok"
+CONF_CONTENT_TYPE = "content_type"
+CONF_LIMIT_REFETCH_TO_URL_CHANGE = "limit_refetch_to_url_change"
+CONF_STILL_IMAGE_URL = "still_image_url"
+CONF_STREAM_SOURCE = "stream_source"
+CONF_FRAMERATE = "framerate"
+CONF_EXTRA_ARGUMENTS = "extra_arguments"
+GET_IMAGE_TIMEOUT = 10
+DEFAULT_ARGUMENTS = "-pred 1"
